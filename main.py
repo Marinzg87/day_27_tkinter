2089225253f96f8c50a1a4df2d827fcb25795646
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-10-07 08:15:16 +0200

    GUI APP Mile to Km Conventer with TKinter.

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My first GUI program with tkinter")
-window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I'm a label", font=("Arial", 14, "bold"))
-my_label.pack()
+def button_clicked():
+    miles = float(mile_entry.get())
+    km = round(miles * 1.609)
+    km_value["text"] = f"{km}"
+
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=300, height=100)
+window.config(padx=10, pady=10)
+
+is_equal = Label(text="is equal to")
+is_equal.grid(row=1, column=0)
+
+mile_entry = Entry(width=7)
+mile_entry.grid(row=0, column=1)
+miles = Label(text="Miles")
+miles.grid(row=0, column=2)
+
+km_value = Label(text="0")
+km_value.grid(row=1, column=1)
+km = Label(text="Km")
+km.grid(row=1, column=2)
+
+my_button = Button(text="Calculate", command=button_clicked)
+my_button.grid(row=2, column=1)
+
 
 window.mainloop()
